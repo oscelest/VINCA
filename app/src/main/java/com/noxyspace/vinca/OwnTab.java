e5d9f700ab35e918d4454e633634e66e4ea2716c
Rune Hou Thode <runehouthode@gmail.com>
2016-10-27 14:28:41 +0200

    Tilføjet ListView

--- app/src/main/java/com/noxyspace/vinca/OwnTab.java
+++ app/src/main/java/com/noxyspace/vinca/OwnTab.java
@@ -1,17 +1,35 @@
 package com.noxyspace.vinca;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
 
-public class OwnTab extends Fragment {
+public class OwnTab extends ListFragment implements AdapterView.OnItemClickListener {
 
+    private String[] listItems = {"Folder 1", "Folder 2", "Folder 2"};
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.tab_fragment_own, container, false);
 
 
-        return inflater.inflate(R.layout.tab_fragment_own, container, false);
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(getActivity(), R.array.Planets, android.R.layout.simple_list_item_1);
+        setListAdapter(adapter);
+        getListView().setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Toast.makeText(getActivity(), "Item: " + position, Toast.LENGTH_SHORT).show();
     }
 }
