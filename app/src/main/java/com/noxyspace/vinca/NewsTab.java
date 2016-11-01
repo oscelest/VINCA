52da5541945a44a849bfac69fc483d5707fbf3fa
Magnus Haakonsson <magnusmisbat@gmail.com>
2016-11-01 19:25:07 +0100

    Begyndt at kigge på baseAdaptor, intet specielt i kommit

--- app/src/main/java/com/noxyspace/vinca/NewsTab.java
+++ app/src/main/java/com/noxyspace/vinca/NewsTab.java
@@ -1,15 +1,27 @@
 package com.noxyspace.vinca;
 
-import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 
-public class NewsTab extends Fragment {
+public class NewsTab extends Fragment implements AdapterView.OnItemClickListener, View.OnClickListener{
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.news_tab_fragment, container, false);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
     }
 }
