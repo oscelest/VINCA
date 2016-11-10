4c3f8215f051e3d6c9c4ba0ec967b635c44d6e9b
Magnus Haakonsson <magnusmisbat@gmail.com>
2016-11-10 14:40:48 +0100

    Added something from the web

--- app/src/main/java/com/noxyspace/vinca/NewsTab.java
+++ app/src/main/java/com/noxyspace/vinca/NewsTab.java
@@ -12,11 +12,21 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 
 public class NewsTab extends ListFragment implements AdapterView.OnItemClickListener {
 
     private ArrayList<SingleRow> newsList = new ArrayList<SingleRow>();
+    private ArrayList<String> titleList = new ArrayList<String>();
+    private ArrayList<String> descriptionList = new ArrayList<String>();
+
+
     private CustomAdapter adapter;
     private int id = 1;
 
@@ -59,11 +69,17 @@ public class NewsTab extends ListFragment implements AdapterView.OnItemClickList
 
         // END DUMMY TEXT
 
-        // Inserting text into Single Row Objects
+        titleList.add(Title1);
+        titleList.add(Title2);
+        titleList.add(Title2);
 
-        newsList.add(new SingleRow(Title1, Description1));
-        newsList.add(new SingleRow(Title2, Description2));
-        newsList.add(new SingleRow(Title3, Description3));
+        descriptionList.add(Description1);
+        descriptionList.add(Description2);
+        descriptionList.add(Description3);
+
+        for ( int i = 0 ; i < titleList.size() ; i++){
+            newsList.add(new SingleRow(titleList.get(i), descriptionList.get(i)));
+        }
 
         return view;
     }
@@ -133,6 +149,49 @@ public class NewsTab extends ListFragment implements AdapterView.OnItemClickList
                 description.setText(newsList.get(position).getDescription());
 
             return view;
+        }
+    }
+
+    // HTTP REQUEST
+
+    public static String executePost(String targetURL, String urlParameters) {
+        HttpURLConnection connection = null;
+
+        try {
+            //Create connection
+            URL url = new URL(targetURL);
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            connection.setRequestProperty("Content-Length", Integer.toString(urlParameters.getBytes().length));
+            connection.setRequestProperty("Content-Language", "en-US");
+
+            connection.setUseCaches(false);
+            connection.setDoOutput(true);
+
+            //Send request
+            DataOutputStream wr = new DataOutputStream (connection.getOutputStream());
+            wr.writeBytes(urlParameters);
+            wr.close();
+
+            //Get Response
+            InputStream is = connection.getInputStream();
+            BufferedReader rd = new BufferedReader(new InputStreamReader(is));
+            StringBuilder response = new StringBuilder(); // or StringBuffer if Java version 5+
+            String line;
+            while ((line = rd.readLine()) != null) {
+                response.append(line);
+                response.append('\r');
+            }
+            rd.close();
+            return response.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            if (connection != null) {
+                connection.disconnect();
+            }
         }
     }
 }
