5157fb9fcd40f513c8e93bfd0fb4d477b487809e
Rune Hou Thode <runehouthode@gmail.com>
2017-01-04 13:28:02 +0100

    Stavefejl

---
+++ app/src/main/java/com/noxyspace/vinca/symbols/Decision.java
@@ -0,0 +1,8 @@
+package com.noxyspace.vinca.symbols;
+
+public class Decision extends Symbol {
+
+    public Decision(int id, String title, String description) {
+        super(id, title, description);
+    }
+}
