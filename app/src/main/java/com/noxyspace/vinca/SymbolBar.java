9690066f991d938cc966ec75d4f74a8ac62783fc
Rune Hou Thode <runehouthode@gmail.com>
2017-01-05 12:56:09 +0100

    Tilføjet symboler til kanvas-baren

--- app/src/main/java/com/noxyspace/vinca/SymbolBar.java
+++ app/src/main/java/com/noxyspace/vinca/SymbolBar.java
@@ -5,17 +5,19 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 
 public class SymbolBar extends Fragment {
+
+    private ImageView projectOpen, projectClose, activity, pause, iteration, process, processCol, question, answer, method;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_symbol_bar, container, false);
 
-
-
+//        projectClose = (ImageView) view.findViewById()
 
         return view;
     }
