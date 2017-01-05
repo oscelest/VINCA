afb50106c1c651ae974befc979a41b5ab3d16daf
Andreas <andreasfyhn@hotmail.com>
2017-01-05 15:58:48 +0100

    Startet implementing drag and drop function

--- app/src/main/java/com/noxyspace/vinca/SymbolBar.java
+++ app/src/main/java/com/noxyspace/vinca/SymbolBar.java
@@ -2,6 +2,7 @@ package com.noxyspace.vinca;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,14 +11,87 @@ import android.widget.ImageView;
 
 public class SymbolBar extends Fragment {
 
-    private ImageView projectOpen, projectClose, activity, pause, iteration, process, processCol, question, answer, method;
+    private ImageView projectStart, projectEnd, activity, pause, iterationStart, iterationEnd, processStart, processEnd, decision, method;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_symbol_bar, container, false);
 
-//        projectClose = (ImageView) view.findViewById()
+        projectStart = (ImageView) view.findViewById(R.id.project_start);
+        projectEnd = (ImageView) view.findViewById(R.id.project_end);
+        activity = (ImageView) view.findViewById(R.id.activity);
+        pause = (ImageView) view.findViewById(R.id.pause);
+        iterationStart = (ImageView) view.findViewById(R.id.iteration_start);
+        iterationEnd = (ImageView) view.findViewById(R.id.iteration_end);
+        processStart = (ImageView) view.findViewById(R.id.process_start);
+        processEnd = (ImageView) view.findViewById(R.id.process_end);
+        decision  = (ImageView) view.findViewById(R.id.decision);
+        method = (ImageView) view.findViewById(R.id.method);
+
+        projectStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //startDrag();
+            }
+        });
+        projectEnd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        activity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        pause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        iterationStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        iterationEnd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        processStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        processEnd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        decision.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        method.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+
 
         return view;
     }
